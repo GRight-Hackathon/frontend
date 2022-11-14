4612d6ccc570e46b9ef07d8c1e8db860b5951e94
Chow Yuan Jing <zcyuanjing@gmail.com>
2022-11-14 13:56:55 +0800

    Chore: Change pictures

--- pages/aboutus.js
+++ pages/aboutus.js
@@ -17,20 +17,20 @@ export default function AboutUs() {
         <Body className="flex flex-col items-center">
             <div class='w-4/5'>
                 <div class="grid grid-cols-3 gap-6">
-                    <Image src={jon} class="col-span-1 object-contain shadow rounded border-none h-30 w-30" />
-                    <Image src={jason} class="col-span-1 object-contain shadow rounded border-none h-30 w-30" />
-                    <Image src={eugene} class="col-span-1 object-contain shadow rounded border-none h-30 w-30" />
+                    <Image src={jon} class="col-span-1 object-contain shadow rounded border-none" />
+                    <Image src={jason} class="col-span-1 object-contain shadow rounded border-none" />
+                    <Image src={eugene} class="col-span-1 object-contain shadow rounded border-none" />
                 </div>
                 <div class="grid grid-cols-3 gap-6 justify-items-center">
                 <div class="text-xl text-gray-800">Jonathan Yap <a href='https://www.linkedin.com/in/jonjon98/'> <Image height={30} width={30} src={linkedin}/> </a> <a href='https://github.com/jonjon98'> <Image height={30} width={30} src={github} /></a></div>
                 <div class="text-xl text-gray-800">Jason Yap <a href='https://www.linkedin.com/in/ja-sony/'> <Image height={30} width={30} src={linkedin}/> </a> <a href='https://github.com/JasonYapzx'> <Image height={30} width={30} src={github} /></a></div>
                 <div class="text-xl text-gray-800">Eugene Tay <a href='https://www.linkedin.com/in/eugenetayyj/'> <Image height={30} width={30} src={linkedin}/> </a> <a href='https://github.com/eugenetayyj'> <Image height={30} width={30} src={github} /></a></div>
                 </div>
-                <div class="grid grid-cols-2 gap-6">
-                    <Image src={yj} class="col-span-1 object-contain shadow rounded border-none h-30 w-30" />
-                    <Image src={shaune} class="col-span-1 object-contain shadow rounded border-none h-30 w-30" />
+                <div class="grid grid-cols-3 gap-6">
+                    <Image src={yj} class="col-span-1 object-contain shadow rounded border-none" />
+                    <Image src={shaune} class="col-span-1 object-contain shadow rounded border-none" />
                 </div>
-                <div class="grid grid-cols-2 gap-6 justify-items-center">
+                <div class="grid grid-cols-3 gap-6 justify-items-center">
                 <div class="text-xl text-gray-800">Chow Yuan Jing <a href='https://www.linkedin.com/in/chow-yuan-jing/'> <Image height={30} width={30} src={linkedin}/> </a> <a href='https://github.com/seewhyjay'> <Image height={30} width={30} src={github} /></a></div>
                 <div class="text-xl text-gray-800">Shaune Ang <a href='https://www.linkedin.com/in/shauneang/'> <Image height={30} width={30} src={linkedin}/> </a> <a href='https://github.com/shauneang'> <Image height={30} width={30} src={github} /></a></div>
                 </div>
